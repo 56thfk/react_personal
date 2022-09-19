a483f95d2f00dce2aa01565db2446bca302b6f25
56thfk <56thfk@naver.com>
2022-09-19 16:01:35 +0900

    pure.html 추가 및 App.js 수정

--- react_native/src/App.js
+++ react_native/src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 컴포넌트 첫 글자는 대문자
+function Subject() {
+  return (
+    <header>
+      <h1>WEB</h1>
+      World Wide Web !
+    </header>
+  );
+}
+
+function TableOfContent() {
+  return(
+    <nav>
+      <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+function Content() {
+  return(
+    <article>
+            <h2>HTML</h2>
+            HTML is HyperText Markup Language.
+    </article>
+  );
+}
+
+// className은 최상위 컴포넌트 이름
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+      <Subject></Subject>
+      <TableOfContent></TableOfContent>
+      <Content></Content>
     </div>
   );
 }
