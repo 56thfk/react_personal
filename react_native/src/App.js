0a2028a6c47cce1bc86818488f8160e31c847b9e
56thfk <56thfk@naver.com>
2022-09-19 16:27:48 +0900

    Component Code 분리

--- react_native/src/App.js
+++ react_native/src/App.js
@@ -1,45 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
-// 컴포넌트 첫 글자는 대문자
-function Subject() {
-  return (
-    <header>
-      <h1>WEB</h1>
-      World Wide Web !
-    </header>
-  );
-}
-
-function TableOfContent() {
-  return(
-    <nav>
-      <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-          
-      </ul>
-    </nav>
-  );
-}
-
-function Content() {
-  return(
-    <article>
-            <h2>HTML</h2>
-            HTML is HyperText Markup Language.
-    </article>
-  );
-}
+import TableOfContent from './components/TableOfContent';
 
 // className은 최상위 컴포넌트 이름
 function App() {
   return (
     <div className="App">  
-      <Subject></Subject>
+      <Subject title = "WEB" subTitle = "World Wide Web !"></Subject>
       <TableOfContent></TableOfContent>
-      <Content></Content>
+      <Content title = "HTML" desc = "HTML is HyperText Markup Language"></Content>
     </div>
   );
 }
