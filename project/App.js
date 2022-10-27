610313e55e9614bfbbc42899b66236a925828e6c
56thfk <56thfk@naver.com>
2022-10-27 19:58:06 +0900

    Test용 JS파일

--- project/App.js
+++ project/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, SafeAreaView } from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
@@ -8,6 +8,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer'
 import Home from './Screen/Home';
 import Sample from './Screen/Sample';
 import Profile from './Screen/Profile';
+import Test from './Screen/Test';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -16,9 +18,11 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar s></StatusBar>
       <Drawer.Navigator>
         <Drawer.Screen name = "Home" component={Home}/>
         <Drawer.Screen name = "Profile" component={Profile}/>
+        <Drawer.Screen name = "Test" component={Test}/>
       </Drawer.Navigator>
     </NavigationContainer>
   );
